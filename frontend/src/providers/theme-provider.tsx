00edc4d4c2c88076454f17a0136c089abb489f8f
Lizzietrust <ohidaelizabeth3@gmail.com>
2026-06-01 11:23:12 +0100

    feat: add ThemeProvider for managing theme context with next-themes

---
+++ frontend/src/providers/theme-provider.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import * as React from "react";
+import {
+  ThemeProvider as NextThemesProvider,
+  ThemeProviderProps as NextThemeProviderProps,
+} from "next-themes";
+
+export type { ThemeProviderProps } from "next-themes";
+
+export function ThemeProvider({ children, ...props }: NextThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
